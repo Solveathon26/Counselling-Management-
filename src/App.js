839bfb27728a9cc85d453594a5c9b1aec4008c72
Naman Srivastava <namansrivastava1608@gmail.com>
2026-04-04 01:58:24 +0530

    fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { Show, RedirectToSignIn } from '@clerk/react';
+import { BrowserRouter as Router, Routes, Route, Navigate, Outlet } from 'react-router-dom';
+import { Show, RedirectToSignIn, ClerkLoaded, ClerkLoading } from '@clerk/react';
+
 import Sidebar from './components/Sidebar';
 import Landing from './components/Landing';
 import Login from './components/Login';
@@ -12,6 +13,7 @@ import StudentSignUp from './components/StudentSignUp';
 import CounsellorSignUp from './components/CounsellorSignUp';
 import ParentSignUp from './components/ParentSignUp';
 import RoleSelection from './components/RoleSelection';
+
 import './index.css';
 
 function App() {
@@ -19,45 +21,55 @@ function App() {
     <Router>
       <div className="app-container">
         <Routes>
+
           {/* Public */}
           <Route path="/" element={<Landing />} />
-          
-          {/* Registration Routes */}
-          <Route path="/sign-up"             element={<RoleSelection />} />
-          <Route path="/sign-up/parent/*"    element={<ParentSignUp />} />
-          <Route path="/warden/signup/*"     element={<StudentSignUp />} />
+
+          {/* Registration */}
+          <Route path="/sign-up" element={<RoleSelection />} />
+          <Route path="/sign-up/parent/*" element={<ParentSignUp />} />
+          <Route path="/warden/signup/*" element={<StudentSignUp />} />
           <Route path="/warden/counsellor/signup/*" element={<CounsellorSignUp />} />
 
-          {/* Clerk Login Routes */}
-          <Route path="/login/student/*"     element={<Login fixedRole="student" redirect="/student" />} />
-          <Route path="/login/warden/*"      element={<Login fixedRole="warden" redirect="/warden" />} />
-          <Route path="/login/counsellor/*"  element={<Login fixedRole="counsellor" redirect="/counsellor" />} />
-          <Route path="/login/parent/*"      element={<Login fixedRole="parent" redirect="/parent" />} />
+          {/* Login */}
+          <Route path="/login/student/*" element={<Login fixedRole="student" redirect="/student" />} />
+          <Route path="/login/warden/*" element={<Login fixedRole="warden" redirect="/warden" />} />
+          <Route path="/login/counsellor/*" element={<Login fixedRole="counsellor" redirect="/counsellor" />} />
+          <Route path="/login/parent/*" element={<Login fixedRole="parent" redirect="/parent" />} />
 
-          {/* Authenticated Routes with Sidebar */}
-          <Route path="/*" element={
-            <>
-              <Show when="signed-in">
-                <Sidebar />
-                <div className="main-content">
-                  <Routes>
-                    <Route path="/student"    element={<StudentDashboard />} />
-                    <Route path="/warden"     element={<WardenDashboard />} />
-                    <Route path="/counsellor" element={<CounsellorDashboard />} />
-                    <Route path="/parent"     element={<ParentDashboard />} />
-                    <Route path="*"           element={<Navigate to="/" replace />} />
-                  </Routes>
-                </div>
-              </Show>
-              <Show when="signed-out">
-                <Routes>
-                   <Route path="/student"    element={<RedirectToSignIn />} />
-                   <Route path="/counsellor" element={<RedirectToSignIn />} />
-                   <Route path="/parent"     element={<RedirectToSignIn />} />
-                </Routes>
-              </Show>
-            </>
-          } />
+          {/* Protected Layout */}
+          <Route
+            path="/"
+            element={
+              <>
+                <ClerkLoading>
+                  <div>Loading...</div>
+                </ClerkLoading>
+
+                <ClerkLoaded>
+                  <Show when="signed-in">
+                    <Sidebar />
+                    <div className="main-content">
+                      <Outlet />
+                    </div>
+                  </Show>
+
+                  <Show when="signed-out">
+                    <RedirectToSignIn />
+                  </Show>
+                </ClerkLoaded>
+              </>
+            }
+          >
+            <Route path="student" element={<StudentDashboard />} />
+            <Route path="warden" element={<WardenDashboard />} />
+            <Route path="counsellor" element={<CounsellorDashboard />} />
+            <Route path="parent" element={<ParentDashboard />} />
+          </Route>
+
+          {/* Fallback */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+
         </Routes>
       </div>
     </Router>
